f83ad7af4b184a9b132e1fdc3441f659e6ff6da4
Akshay5312 <40531389+Akshay5312@users.noreply.github.com>
2024-01-27 16:48:03 -0500

    Create save_last_solution.h
    a simple way to save a solution as comma separated values to a text file

---
+++ helpers/include/save_last_solution.h
@@ -0,0 +1,37 @@
+#include <casadi/casadi.hpp>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+
+/**
+ * @brief Saves the last solution as a CSV to a file in the form "t,x1,x2...,xn" where each row corresponds to a unique time step
+ * 
+ * @param last_solution The last solution from the solver
+ * @param folder_location The location of the folder to save the file to
+*/
+void save_last_solution_to_file(casadi::MXVector& last_solution, std::string folder_location){
+    std::ofstream file;
+    file.open(folder_location + "/last_solution.csv");
+
+    std::vector<double> times = traj.get_global_times();
+
+    auto solx = last_solution[0];
+
+    syd::vector<std::vector<double>> x_all;
+    for(int i = 0; i < solx.rows(); i++){
+        std::vector<double> xi_all = casadi::MX::evalf(solx(i,casadi::Slice(0, solx.columns()))).get_elements();
+        x_all.push_back(xi_all);
+    }
+
+    for(int i = 0; i < x_all.size(); i++){
+        file << times[i] << ", ";
+        for(int j = 0; j < x_all[i].size()-1; j++){
+            file << x_all[i][j] << ", ";
+        }
+        file << x_all[i][x_all[i].size()-1];
+        file << std::endl;
+    }   
+
+    file.close();
+};
