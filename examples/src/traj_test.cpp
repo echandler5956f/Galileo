0162e02c0a291441fac77a3ba2e8c245c64c4c73
echandler5956f <ethanchandler5956f@gmail.com>
2024-02-02 14:39:21 -0500

    Fixed namespaces

--- examples/src/traj_test.cpp
+++ examples/src/traj_test.cpp
@@ -14,11 +14,11 @@ int main(int argc, char **argv)
     Data data = Data(model);
 
     // Create environment Surfaces
-    shared_ptr<environment::EnvironmentSurfaces> surfaces = make_shared<environment::EnvironmentSurfaces>();
+    std::shared_ptr<environment::EnvironmentSurfaces> surfaces = std::make_shared<environment::EnvironmentSurfaces>();
     surfaces->push_back(environment::CreateInfiniteGround());
     std::cout << "EnvironmentSurfaces created" << std::endl;
 
-    shared_ptr<contact::ContactSequence> contact_sequence = make_shared<contact::ContactSequence>(num_ees);
+    std::shared_ptr<contact::ContactSequence> contact_sequence = std::make_shared<contact::ContactSequence>(num_ees);
     std::cout << "ContactSequence created" << std::endl;
 
     legged::contact::RobotEndEffectors ees = bot.getEndEffectors();
@@ -30,13 +30,13 @@ int main(int argc, char **argv)
 
     auto nq = model.nq;
     auto nv = model.nv;
-    shared_ptr<opt::LeggedRobotStates> si = make_shared<opt::LeggedRobotStates>(nq, nv, ees);
+    std::shared_ptr<opt::LeggedRobotStates> si = std::make_shared<opt::LeggedRobotStates>(nq, nv, ees);
     std::cout << "LeggedRobotStates created" << std::endl;
 
     contact::ContactMode initial_mode;
     initial_mode.combination_definition = bot.getContactCombination(0b11);
     initial_mode.contact_surfaces = {0, 0};
-    initial_mode.createModeDynamics(make_shared<Model>(model), ees, si);
+    initial_mode.createModeDynamics(model, ees, si);
     std::cout << "Initial mode created" << std::endl;
 
     contact_sequence->addPhase(initial_mode, 100, 0.2);
@@ -46,11 +46,11 @@ int main(int argc, char **argv)
     initial_mode.getModeDynamics(F);
     std::cout << "Initial mode dynamics created" << std::endl;
 
-    SX cx = SX::sym("x", si->nx);
-    SX cx2 = SX::sym("x2", si->nx);
-    SX cdx = SX::sym("dx", si->ndx);
-    SX cu = SX::sym("u", si->nu);
-    SX cdt = SX::sym("dt");
+    casadi::SX cx = casadi::SX::sym("x", si->nx);
+    casadi::SX cx2 = casadi::SX::sym("x2", si->nx);
+    casadi::SX cdx = casadi::SX::sym("dx", si->ndx);
+    casadi::SX cu = casadi::SX::sym("u", si->nu);
+    casadi::SX cdt = casadi::SX::sym("dt");
 
     auto ch = si->get_ch(cx);
     auto ch_d = si->get_ch_d(cdx);
@@ -71,12 +71,12 @@ int main(int argc, char **argv)
     auto cv2 = si->get_v(cx2);
 
     ConfigVectorAD q_AD(model.nq);
-    q_AD = Eigen::Map<ConfigVectorAD>(static_cast<vector<ADScalar>>(cq).data(), model.nq, 1);
+    q_AD = Eigen::Map<ConfigVectorAD>(static_cast<std::vector<ADScalar>>(cq).data(), model.nq, 1);
     
     ConfigVectorAD q2_AD(model.nq);
-    q2_AD = Eigen::Map<ConfigVectorAD>(static_cast<vector<ADScalar>>(cq2).data(), model.nq, 1);
+    q2_AD = Eigen::Map<ConfigVectorAD>(static_cast<std::vector<ADScalar>>(cq2).data(), model.nq, 1);
 
-    Function Fint("Fint",
+    casadi::Function Fint("Fint",
                   {cx, cdx, cdt},
                   {vertcat(ch + ch_d,
                            cdh + cdh_d,
@@ -85,38 +85,35 @@ int main(int argc, char **argv)
                            cv + cv_d)});
 
     ConfigVectorAD v_result = pinocchio::difference(cmodel, q_AD, q2_AD);
-    SX cv_result(model.nv, 1);
+    casadi::SX cv_result(model.nv, 1);
     pinocchio::casadi::copy(v_result, cv_result);
 
-    Function Fdif("Fdif",
+    casadi::Function Fdif("Fdif",
                   {cx, cx2, cdt},
                   {vertcat(ch2 - ch,
                            cdh2 - cdh,
                            cv_result / cdt,
                            cv2 - cv)});
-    SX cq0(model.nq);
+    casadi::SX cq0(model.nq);
     pinocchio::casadi::copy(q0_vec, cq0);
 
-    Function L("L",
+    casadi::Function L("L",
                {cx, cu},
-               {1e-3 * SX::sumsqr(cvju) +
-                1e-4 * SX::sumsqr(cwrenches) +
-                1e1 * SX::sumsqr(cqj - cq0(Slice(7, nq)))});
+               {1e-3 * casadi::SX::sumsqr(cvju) +
+                1e-4 * casadi::SX::sumsqr(cwrenches) +
+                1e1 * casadi::SX::sumsqr(cqj - cq0(casadi::Slice(7, nq)))});
 
-    Function Phi("Phi",
+    casadi::Function Phi("Phi",
                  {cx},
-                 {1e2 * SX::sumsqr(cqj - cq0(Slice(7, nq)))});
+                 {1e2 * casadi::SX::sumsqr(cqj - cq0(casadi::Slice(7, nq)))});
 
-    Dict opts;
+    casadi::Dict opts;
     opts["ipopt.linear_solver"] = "ma97";
     opts["ipopt.ma97_order"] = "metis";
     opts["ipopt.fixed_variable_treatment"] = "make_constraint";
-    opts["ipopt.max_iter"] = 25;
+    opts["ipopt.max_iter"] = 5;
 
-    using namespace legged;
-    using namespace constraints;
-
-    shared_ptr<GeneralProblemData> gp_data = make_shared<GeneralProblemData>(Fint, Fdif, F, L, Phi);
+    std::shared_ptr<GeneralProblemData> gp_data = std::make_shared<GeneralProblemData>(Fint, Fdif, F, L, Phi);
 
     std::shared_ptr<ConstraintBuilder<LeggedRobotProblemData>> friction_cone_constraint_builder =
         std::make_shared<FrictionConeConstraintBuilder<LeggedRobotProblemData>>();
@@ -127,9 +124,9 @@ int main(int argc, char **argv)
     std::shared_ptr<ConstraintBuilder<LeggedRobotProblemData>> contact_constraint_builder =
         std::make_shared<ContactConstraintBuilder<LeggedRobotProblemData>>();
 
-    vector<shared_ptr<ConstraintBuilder<LeggedRobotProblemData>>> builders = {};//friction_cone_constraint_builder, velocity_constraint_builder, contact_constraint_builder};
+    std::vector<std::shared_ptr<ConstraintBuilder<LeggedRobotProblemData>>> builders = {friction_cone_constraint_builder, velocity_constraint_builder, contact_constraint_builder};
 
-    shared_ptr<LeggedRobotProblemData> legged_problem_data = make_shared<LeggedRobotProblemData>(gp_data, surfaces, contact_sequence, si, make_shared<ADModel>(cmodel), make_shared<ADData>(cdata), ees, cx, cu, cdt, 20);
+    std::shared_ptr<LeggedRobotProblemData> legged_problem_data = std::make_shared<LeggedRobotProblemData>(gp_data, surfaces, contact_sequence, si, std::make_shared<ADModel>(cmodel), std::make_shared<ADData>(cdata), ees, cx, cu, cdt, 20);
 
     std::vector<opt::ConstraintData> constraint_datas;
     for (auto builder : builders)
@@ -143,7 +140,7 @@ int main(int argc, char **argv)
 
     TrajectoryOpt<LeggedRobotProblemData> traj(legged_problem_data, builders, opts);
 
-    DM X0 = DM::zeros(si->nx, 1);
+    casadi::DM X0 = casadi::DM::zeros(si->nx, 1);
     int j = 0;
     for (int i = si->nh + si->ndh; i < si->nh + si->ndh + si->nq; ++i)
     {
