e3d51c1d1c0cc6c6f1f3f31e5cd50b376f2c7758
Akshay5312 <akshayjaitly@hotmail.com>
2024-03-13 17:33:35 -0400

    Creating a Legged Interface

--- galileo/legged-model/include/galileo/legged-model/VelocityConstraintBuilder.h
+++ galileo/legged-model/include/galileo/legged-model/VelocityConstraintBuilder.h
@@ -125,7 +125,7 @@ namespace galileo
             {
                 std::shared_ptr<environment::EnvironmentSurfaces> environment_surfaces;
                 std::shared_ptr<contact::ContactSequence> contact_sequence;
-                std::shared_ptr<opt::LeggedRobotStates> states;
+                std::shared_ptr<legged::LeggedRobotStates> states;
                 std::shared_ptr<opt::ADModel> ad_model;
                 std::shared_ptr<opt::ADData> ad_data;
                 contact::RobotEndEffectors robot_end_effectors;
